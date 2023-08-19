9bb6d9a819a27e77e57432a21992040e23e834e5
Melisa Budini <devmelb23@gmail.com>
2023-08-19 14:48:52 -0300

    PreEntrega2

--- main.js
+++ main.js
@@ -1,64 +1,47 @@
-function saludar(){
-    let nombre = prompt("Ingresa tu nombre!");
-    alert(`Hola ${nombre} ahora podrás elegir qué libro quieres`);
-    
-}
-saludar ();
-let precio = 0; 
-function busca(libro, precio){
-    function envio(region, precio){
-        const suma  = (a,b) => a + b;
-        const resta = (a,b) => a - b;
-        // const iva   = x => x * 0.19;
-        let descuento = 3000 
-        let precioEnvio = 5000;
-        if(region === 1){
-            let nuevoPrecio = resta(suma(precio, precioEnvio), descuento);
-            alert(`El libro que buscas es ${libro} y su precio con envío es $${nuevoPrecio}`);
-        } else {
-            let nuevoPrecio = (suma(precio, precioEnvio));
-            alert(`El libro que buscas es ${libro} y su precio con envío es $${nuevoPrecio}`);
+
+let nombrePersona = prompt("Ingresa tu nombre!");
+alert(`Hola ${nombrePersona} ahora podrás buscar el libro quieres`);
+const biblioteca = [
+    {id:1, nombre: "Persuasión", precio:8990, autor: "Jane Austen" },
+    {id:2, nombre: "Diez Negritos", precio:12990, autor: "Agatha Christie"},
+    {id:3, nombre: "Estudio en escarlata", precio:7900, autor: "Arthur Conan Doyle" },
+    {id:4, nombre: "Harry Potter y la Cámara de los secretos", precio:14500, autor: "J. K. Rowling"}
+];
+
+let nombre = prompt(` ${nombrePersona}, Ingresa el nombre del libro que buscas o escribe ESC para salir`);
+
+while(nombre!="ESC"){
+const book = biblioteca.find((item) => (item.nombre).toLowerCase() === nombre.toLowerCase());
+
+if (book){
+    let mensaje = `
+    Nombre: ${book.nombre}
+    Precio: $${book.precio}`;
+alert(mensaje);
+let region = parseInt(prompt("Ingresa el número de la región donde vives: 1 para Santiago o 2 otra region del país"));
+envio(region, book.precio, book.nombre);
+function envio(region, precio, nombre){
+            const suma  = (a,b) => a + b;
+            const resta = (a,b) => a - b;
+            const iva   = x => x * 0.19;
+            let descuento = 3000 
+            let precioEnvio = 5000;
+            if(region === 1){
+                let nuevoPrecio = resta(suma(precio, precioEnvio, iva(precio)), descuento);
+                alert(`El libro que buscas es ${book.nombre} y su precio con envío es $${nuevoPrecio}`);
+            } else if(region ===2){
+                let nuevoPrecio = (suma(precio, precioEnvio, iva(precio)));
+                alert(`El libro que buscas es ${book.nombre} y su precio con envío es $${nuevoPrecio}`);
+            }else{
+                alert(`Recuerda que no tenemos envío internacional `);
+                let region = parseInt(prompt("Ingresa el número de la región donde vives: 1 para Santiago o 2 otra region del país"));
+                envio(region, precio, nombre);
+            }
+          
+          
         }
-      
-    }
-    
-    if(libro === 1){
-        libro = "Harry Potter y la Piedra Filosofal";
-        precio= 18000;
-        alert(`El libro que buscas es ${libro} y su precio es $${precio}`)
-        let region = parseInt(prompt("Ingresa el número de la región donde vives: 1 para Santiago o 2 otra region del país"));
-        envio(region, precio, libro);
- 
-    } else if(libro === 2) {
-        libro = "Cazadores de sombras";
-        precio = 25000;
-        alert(`El libro que buscas es ${libro} y su precio es $${precio}`)
-        let region = parseInt(prompt("Ingresa el número de la región donde vives: 1 para Santiago o 2 otra region del país"));
-        envio(region, precio, libro);
-      
-    } else if ( libro === 3) {
-        libro = "Orgullo y Prejucio";
-        precio = 9990;
-        alert(`El libro que buscas es ${libro} y su precio es $${precio}`)
-        let region = parseInt(prompt("Ingresa el número de la región donde vives: 1 para Santiago o 2 otra region del país"));
-        envio(region, precio, libro);
-    }else {
-       alert("Ingrese de nuevo su opción")
-    }
-       
+} else {
+    alert("libro no encontrado");
 }
-      
-
-
-let libro = parseInt(prompt("Ingresa el número del libro que buscas: 1: Harry Potter y la Piedra Filosofal, 2: Cazadores de sombras o 3: Orgullo y Prejucio"))
-
-
-
-busca(libro);
-
-
-
-
-
-
-
+nombre = prompt(`${nombrePersona}, Ingresa el nombre del libro que buscas o escribe ESC para salir`);
+}
