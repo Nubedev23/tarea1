82870b8f00d838b58199ca3248150c438ed95b73
Melisa Budini <devmelb23@gmail.com>
2023-08-19 16:05:00 -0300

    preentrega2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ alert(`Hola ${nombrePersona} ahora podrás buscar el libro quieres`);
 const biblioteca = [
     {id:1, nombre: "Persuasión", precio:8990, autor: "Jane Austen" },
     {id:2, nombre: "Diez Negritos", precio:12990, autor: "Agatha Christie"},
-    {id:3, nombre: "Estudio en escarlata", precio:7900, autor: "Arthur Conan Doyle" },
+    {id:3, nombre: "Estudio en escarlata", precio:6900, autor: "Arthur Conan Doyle" },
     {id:4, nombre: "Harry Potter y la Cámara de los secretos", precio:14500, autor: "J. K. Rowling"},
     {id:5, nombre: "Orgullo y Prejuicio", precio:9990, autor: "Jane Austen" },
     {id:6, nombre: "Harry Potter y la Piedra Filosofal", precio:13500, autor: "J. K. Rowling"}
@@ -20,20 +20,20 @@ if (book){
     Nombre: ${book.nombre}
     Precio: $${book.precio}`;
 alert(mensaje);
-let region = parseInt(prompt(` ${nombrePersona}, ingresa el número de la región donde vives: 1 para Santiago o 2 otra region del país`));
+let region = parseInt(prompt(` ${nombrePersona}, ingresa el número de la región donde vives: 1 para Santiago o 2 si vives en otra región del país`));
 envio(region, book.precio, book.nombre);
 function envio(region, precio, nombre){
-            const suma  = (a,b) => a + b;
+            const suma  = (a,b, c) => a + b + c;
             const resta = (a,b) => a - b;
             const iva   = x => x * 0.19;
             let descuento = 3000 
             let precioEnvio = 5000;
             if(region === 1){
                 let nuevoPrecio = resta(suma(precio, precioEnvio, iva(precio)), descuento);
-                alert(`El libro que buscas es ${book.nombre} y su precio con envío es $${nuevoPrecio}`);
+                alert(`El libro que buscas es ${nombre} y su precio con iva y envío incluido es $${Math.round(nuevoPrecio)}`);
             } else if(region ===2){
                 let nuevoPrecio = (suma(precio, precioEnvio, iva(precio)));
-                alert(`El libro que buscas es ${book.nombre} y su precio con envío es $${nuevoPrecio}`);
+                alert(`El libro que buscas es ${nombre} y su precio con iva y envío incluido es $${Math.round(nuevoPrecio)}`);
             }else{
                 alert(`Recuerda que no tenemos envío internacional `);
                 let region = parseInt(prompt("Ingresa el número de la región donde vives: 1 para Santiago o 2 otra region del país"));
